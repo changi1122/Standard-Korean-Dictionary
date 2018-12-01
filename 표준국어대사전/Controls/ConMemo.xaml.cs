c8f02015cb0397c1160672d7cc8f2768f56ef926
Woochang Lee <changi112242@outlook.com>
2018-12-01 17:13:29 +0900

    Version 1.7.1.0 Apply reactive design(for small window). Add memo function. Apply pivot control to Adjustment page. Revise network checking logic. Load word data once. Add inform Button. Revise Popup Webview.
    Version 1.7.2.0
    Add appbarbutton tooltip.

---
+++ 표준국어대사전/Controls/ConMemo.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.ApplicationModel.DataTransfer;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// 사용자 정의 컨트롤 항목 템플릿에 대한 설명은 https://go.microsoft.com/fwlink/?LinkId=234236에 나와 있습니다.
+
+namespace 표준국어대사전.Controls
+{
+    public sealed partial class ConMemo : UserControl
+    {
+        public ConMemo()
+        {
+            this.InitializeComponent();
+        }
+
+        private void BtnMemoCopy_Click(object sender, RoutedEventArgs e)
+        {
+            DataPackage dataPackage = new DataPackage { RequestedOperation = DataPackageOperation.Copy };
+            dataPackage.SetText(TextboxMemoBox.Text);
+            Clipboard.SetContent(dataPackage);
+        }
+
+        private void BtnMemoDelete_Click(object sender, RoutedEventArgs e)
+        {
+            TextboxMemoBox.Text = "";
+        }
+    }
+}
