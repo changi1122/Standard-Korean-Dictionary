d0281efdaeef339662c878ac6bba185dad3ec61b
changi1122 <changi112242@gmail.com>
2021-09-18 21:11:11 +0900

    단어 검색 시 더 보기 버튼 위치 및 방식 수정

--- 표준국어대사전/Classes/HistoryManager.cs
+++ 표준국어대사전/Classes/HistoryManager.cs
@@ -2,9 +2,7 @@
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Linq;
-using System.Reflection.Metadata.Ecma335;
-using System.Text;
-using System.Threading.Tasks;
+using Windows.UI.Xaml;
 
 namespace 표준국어대사전.Classes
 {
@@ -15,10 +13,10 @@ namespace 표준국어대사전.Classes
         public bool CanGoBack { get { return (BackStack.Count > 0); } }
         public bool CanGoForward { get { return (ForwardStack.Count > 0); } }
 
-        //이전으로 되돌리기 위한 스택
+        // 이전으로 되돌리기 위한 스택
         LinkedList<RecordPoint> BackStack;
 
-        //앞으로 되돌리기 위한 스택
+        // 앞으로 되돌리기 위한 스택
         LinkedList<RecordPoint> ForwardStack;
 
 
@@ -32,9 +30,9 @@ namespace 표준국어대사전.Classes
         /// <summary>
         /// 검색 결과와 뜻풀이가 모두 변화될 때 이전 값을 기록
         /// </summary>
-        public void RecordAll(string searchText, ObservableCollection<SearchResultItem> searchResults, WordDetailItem definition, int selectedIndex = -1, bool isRedo = false)
+        public void RecordAll(string searchText, string lastSearchText, ObservableCollection<SearchResultItem> searchResults, WordDetailItem definition, int selectedIndex = -1, bool isRedo = false, Visibility isMoreButtonVisible = Visibility.Collapsed)
         {
-            RecordPoint record = new RecordPoint(RecordType.ClearAll, searchText, searchResults, definition, selectedIndex);
+            RecordPoint record = new RecordPoint(RecordType.ClearAll, searchText, lastSearchText, searchResults, definition, selectedIndex, isMoreButtonVisible);
             BackStack.AddLast(record);
             if (BackStack.Count > RECORD_LIMIT)
                 BackStack.RemoveFirst();
@@ -48,9 +46,9 @@ namespace 표준국어대사전.Classes
         /// <summary>
         /// 뜻풀이가 변화될 때 이전 값을 기록
         /// </summary>
-        public void RecordDefinition(string searchText, WordDetailItem definition, int selectedIndex = -1, bool isRedo = false)
+        public void RecordDefinition(string searchText, string lastSearchText, WordDetailItem definition, int selectedIndex = -1, bool isRedo = false, Visibility isMoreButtonVisible = Visibility.Collapsed)
         {
-            RecordPoint record = new RecordPoint(RecordType.ClearDefinition, searchText, null, definition, selectedIndex);
+            RecordPoint record = new RecordPoint(RecordType.ClearDefinition, searchText, lastSearchText, null, definition, selectedIndex, isMoreButtonVisible);
             BackStack.AddLast(record);
             if (BackStack.Count > RECORD_LIMIT)
                 BackStack.RemoveFirst();
@@ -64,7 +62,7 @@ namespace 표준국어대사전.Classes
         /// <summary>
         /// 되돌리기
         /// </summary>
-        public void Undo(ref string searchText, ref ObservableCollection<SearchResultItem> searchResults, ref WordDetailItem definition, ref int selectedIndex)
+        public void Undo(ref string searchText, ref string lastSearchText, ref ObservableCollection<SearchResultItem> searchResults, ref WordDetailItem definition, ref int selectedIndex, ref Visibility isMoreButtonVisible)
         {
             if (BackStack.Count == 0)
                 return;
@@ -74,9 +72,10 @@ namespace 표준국어대사전.Classes
 
             if (previousRecord.Type == RecordType.ClearDefinition)
             {
-                RecordUndoDefinition(searchText, definition, selectedIndex);
+                RecordUndoDefinition(searchText, lastSearchText, definition, selectedIndex, isMoreButtonVisible);
 
                 searchText = previousRecord.SearchText;
+                lastSearchText = previousRecord.LastSearchText;
                 selectedIndex = previousRecord.SelectedIndex;
                 if (previousRecord.Definition != null)
                 {
@@ -85,9 +84,10 @@ namespace 표준국어대사전.Classes
             }
             else if (previousRecord.Type == RecordType.ClearAll)
             {
-                RecordUndoAll(searchText, searchResults, definition, selectedIndex);
+                RecordUndoAll(searchText, lastSearchText, searchResults, definition, selectedIndex, isMoreButtonVisible);
 
                 searchText = previousRecord.SearchText;
+                lastSearchText = previousRecord.LastSearchText;
                 selectedIndex = previousRecord.SelectedIndex;
                 if (previousRecord.SearchResults != null)
                 {
@@ -101,17 +101,18 @@ namespace 표준국어대사전.Classes
                 {
                     definition = previousRecord.Definition;
                 }
+                isMoreButtonVisible = previousRecord.IsMoreButtonVisible;
             }
             else if (previousRecord.Type == RecordType.ClearSearchResult)
             {
-                //일어나지 않는 케이스
+                // 일어나지 않는 케이스
             }
         }
 
         /// <summary>
         /// 다시 실행
         /// </summary>
-        public void Redo(ref string searchText, ref ObservableCollection<SearchResultItem> searchResults, ref WordDetailItem definition, ref int selectedIndex)
+        public void Redo(ref string searchText, ref string lastSearchText, ref ObservableCollection<SearchResultItem> searchResults, ref WordDetailItem definition, ref int selectedIndex, ref Visibility isMoreButtonVisible)
         {
             if (ForwardStack.Count == 0)
                 return;
@@ -121,9 +122,10 @@ namespace 표준국어대사전.Classes
 
             if (postRecord.Type == RecordType.ClearDefinition)
             {
-                RecordDefinition(searchText, definition, selectedIndex, true);
+                RecordDefinition(searchText, lastSearchText, definition, selectedIndex, true, isMoreButtonVisible);
 
                 searchText = postRecord.SearchText;
+                lastSearchText = postRecord.LastSearchText;
                 selectedIndex = postRecord.SelectedIndex;
                 if (postRecord.Definition != null)
                 {
@@ -132,9 +134,10 @@ namespace 표준국어대사전.Classes
             }
             else if (postRecord.Type == RecordType.ClearAll)
             {
-                RecordAll(searchText, searchResults, definition, selectedIndex, true);
+                RecordAll(searchText, lastSearchText, searchResults, definition, selectedIndex, true, isMoreButtonVisible);
 
                 searchText = postRecord.SearchText;
+                lastSearchText = postRecord.LastSearchText;
                 selectedIndex = postRecord.SelectedIndex;
                 if (postRecord.SearchResults != null)
                 {
@@ -148,10 +151,11 @@ namespace 표준국어대사전.Classes
                 {
                     definition = postRecord.Definition;
                 }
+                isMoreButtonVisible = postRecord.IsMoreButtonVisible;
             }
             else if (postRecord.Type == RecordType.ClearSearchResult)
             {
-                //일어나지 않는 케이스
+                // 일어나지 않는 케이스
             }
         }
 
@@ -159,9 +163,9 @@ namespace 표준국어대사전.Classes
         /// <summary>
         /// 되돌리기로 검색 결과와 뜻풀이가 모두 변화될 때 다시 실행을 위해 현재 값을 기록(되돌리기 이전 실행)
         /// </summary>
-        void RecordUndoAll(string searchText, ObservableCollection<SearchResultItem> searchResults, WordDetailItem definition, int selectedIndex = -1)
+        void RecordUndoAll(string searchText, string lastSearchText, ObservableCollection<SearchResultItem> searchResults, WordDetailItem definition, int selectedIndex = -1, Visibility isMoreButtonVisible = Visibility.Collapsed)
         {
-            RecordPoint record = new RecordPoint(RecordType.ClearAll, searchText, searchResults, definition, selectedIndex);
+            RecordPoint record = new RecordPoint(RecordType.ClearAll, searchText, lastSearchText, searchResults, definition, selectedIndex, isMoreButtonVisible);
             ForwardStack.AddLast(record);
             if (ForwardStack.Count > RECORD_LIMIT)
                 ForwardStack.RemoveFirst();
@@ -170,9 +174,9 @@ namespace 표준국어대사전.Classes
         /// <summary>
         /// 되돌리기로 뜻풀이가 변화될 때 다시 실행을 위해 현재 값을 기록(되돌리기 이전 실행)
         /// </summary>
-        void RecordUndoDefinition(string searchText, WordDetailItem definition, int selectedIndex = -1)
+        void RecordUndoDefinition(string searchText, string lastSearchText, WordDetailItem definition, int selectedIndex = -1, Visibility isMoreButtonVisible = Visibility.Collapsed)
         {
-            RecordPoint record = new RecordPoint(RecordType.ClearDefinition, searchText, null, definition, selectedIndex);
+            RecordPoint record = new RecordPoint(RecordType.ClearDefinition, searchText, lastSearchText, null, definition, selectedIndex, isMoreButtonVisible);
             ForwardStack.AddLast(record);
             if (ForwardStack.Count > RECORD_LIMIT)
                 ForwardStack.RemoveFirst();
