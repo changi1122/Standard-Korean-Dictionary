6c66bc9dd25243ce7c916b4f4cd6b838bb6e2d0f
changi1122 <changi112242@gmail.com>
2022-02-03 15:30:36 +0900

    다중 검색 기능 삭제, 웹뷰 관련 디자인 정리

--- 표준국어대사전/Views/Search.xaml.cs
+++ 표준국어대사전/Views/Search.xaml.cs
@@ -43,117 +43,10 @@ namespace 표준국어대사전.Views
         }
 
         /// <summary>
-        /// 단어 검색시 실행
+        /// 반응형 레이아웃 적용
         /// </summary>
-        private async void SearchBox_QuerySubmitted(AutoSuggestBox sender, AutoSuggestBoxQuerySubmittedEventArgs args)
-        {
-            if (SearchBox.Text == "")
-            {
-                var res = ResourceLoader.GetForCurrentView();
-                var messageDialog = new MessageDialog(res.GetString("DA_NoSearchText"));
-                await messageDialog.ShowAsync();
-                return;
-            }
-
-            ViewModel.SearchWords();
-        }
-
-        private void SearchBox_KeyDown(object sender, KeyRoutedEventArgs e)
-        {
-            if (e.Key == Windows.System.VirtualKey.Enter)
-                SearchBox_QuerySubmitted(SearchBox, new AutoSuggestBoxQuerySubmittedEventArgs());
-        }
-
-        /// <summary>
-        /// 검색 결과 리스트뷰의 항목을 클릭시 실행
-        /// </summary>
-        private void ListviewSearchResult_ItemClick(object sender, ItemClickEventArgs e)
-        {
-            var clickedItem = e.ClickedItem as SearchResultItem;
-
-            ViewModel.DisplayWordDetail(clickedItem);
-        }
-
-
-        private void BtnSubSearchClose_Click(object sender, RoutedEventArgs e)
-        {
-            BasicGrid.Children.Remove((UIElement)FindName("SubGrid"));
-            IsWebViewOpen = false;
-        }
-
-        private void WebViewOpen(Uri uri)
-        {
-            if (IsWebViewOpen == true)
-            {
-                Grid g = (Grid)BasicGrid.FindName("SubGrid");
-                WebView w = (WebView)g.FindName("SubSearch");
-                w.Navigate(uri);
-            }
-            else
-            {
-                var SubGrid = new Grid
-                {
-                    Name = "SubGrid",
-                    Margin = new Thickness(0, 40, 0, 0),
-                    VerticalAlignment = VerticalAlignment.Stretch,
-                    HorizontalAlignment = HorizontalAlignment.Stretch,
-                    Background = new SolidColorBrush(Windows.UI.Colors.White)
-                };
-
-                var ColorBar = new Windows.UI.Xaml.Shapes.Rectangle
-                {
-                    Margin = new Thickness(0, 0, 0, 0),
-                    VerticalAlignment = VerticalAlignment.Top,
-                    HorizontalAlignment = HorizontalAlignment.Stretch,
-                    Height = 40,
-                    Fill = (SolidColorBrush)Resources["BarColor"]
-                };
-                SubGrid.Children.Add(ColorBar);
-
-                var CloseBtn = new Button
-                {
-                    Name = "BtnSubSearchClose",
-                    VerticalAlignment = VerticalAlignment.Top,
-                    HorizontalAlignment = HorizontalAlignment.Right,
-                    Height = 40,
-                    Width = 40,
-                    FontSize = 20,
-                    Content = "",
-                    FontFamily = new FontFamily("Segoe MDL2 Assets"),
-                    Background = null,
-                    Foreground = new SolidColorBrush(Windows.UI.Colors.Black)
-                };
-                CloseBtn.Click += BtnSubSearchClose_Click;
-                SubGrid.Children.Add(CloseBtn);
-
-                var SubSearch = new WebView
-                {
-                    Name = "SubSearch",
-                    Margin = new Thickness(1, 40, 1, 1),
-                    VerticalAlignment = VerticalAlignment.Stretch,
-                    HorizontalAlignment = HorizontalAlignment.Stretch,
-                    Source = uri
-                };
-                SubGrid.Children.Add(SubSearch);
-
-                BasicGrid.Children.Add(SubGrid);
-                IsWebViewOpen = true;
-            }
-        }
-
-        private void BtnInform_Click(object sender, RoutedEventArgs e)
-        {
-            WebViewOpen(new Uri("https://stdict.korean.go.kr/help/popup/entry.do"));
-        }
-
-        private async void BtnHelp_Click(object sender, RoutedEventArgs e)
-        {
-            await Windows.System.Launcher.LaunchUriAsync(new Uri("https://costudio1122.blogspot.com/p/blog-page_76.html"));
-        }
-
         private void BasicGrid_SizeChanged(object sender, SizeChangedEventArgs e)
         {
-            // 반응형
             if (BasicGrid.ActualWidth >= 686)
             {
                 // 큰 창일 때
@@ -194,63 +87,113 @@ namespace 표준국어대사전.Views
             ViewModel.IsTitleBarBackButtonEnabled = false;
         }
 
-        private void BtnMultiSearch_Click(object sender, RoutedEventArgs e)
+        /// <summary>
+        /// 단어 검색시 실행
+        /// </summary>
+        private async void SearchBox_QuerySubmitted(AutoSuggestBox sender, AutoSuggestBoxQuerySubmittedEventArgs args)
         {
-            if (BasicGrid.FindName("MultiSearchGrid") != null)
+            if (SearchBox.Text == "")
             {
-                BtnMultiSearchClose_Click(sender, e);
+                var res = ResourceLoader.GetForCurrentView();
+                var messageDialog = new MessageDialog(res.GetString("DA_NoSearchText"));
+                await messageDialog.ShowAsync();
                 return;
             }
 
-            var MultiSearchGrid = new Grid
-            {
-                Name = "MultiSearchGrid",
-                Margin = new Thickness(0, 40, 0, 0),
-                VerticalAlignment = VerticalAlignment.Stretch,
-                HorizontalAlignment = HorizontalAlignment.Right,
-                Background = (SolidColorBrush)Resources["BarColor"],
-                Width = MULTISEARCHGRID_WIDTH
-            };
-
-            if (BasicGrid.ActualWidth < 686)
-            {
-                MultiSearchGrid.HorizontalAlignment = HorizontalAlignment.Stretch;
-                MultiSearchGrid.ClearValue(WidthProperty);
-            }
-
-            var CloseBtn = new Button
-            {
-                Name = "BtnMultiSearchClose",
-                VerticalAlignment = VerticalAlignment.Top,
-                HorizontalAlignment = HorizontalAlignment.Right,
-                Height = 40,
-                Width = 40,
-                FontSize = 20,
-                Content = "",
-                FontFamily = new FontFamily("Segoe MDL2 Assets"),
-                Background = null,
-                Foreground = new SolidColorBrush(Windows.UI.Colors.Black)
-            };
-            CloseBtn.Click += BtnMultiSearchClose_Click;
-            MultiSearchGrid.Children.Add(CloseBtn);
-
-            var MultiSearchFrame = new Frame
-            {
-                Name = "MultiSearchFrame",
-                Margin = new Thickness(1, 40, 1, 1),
-                VerticalAlignment = VerticalAlignment.Stretch,
-                HorizontalAlignment = HorizontalAlignment.Stretch,
-            };
-            MultiSearchFrame.Navigate(typeof(Views.Search));
-            MultiSearchGrid.Children.Add(MultiSearchFrame);
-
-            BasicGrid.Children.Add(MultiSearchGrid);
+            ViewModel.SearchWords();
         }
 
-        private void BtnMultiSearchClose_Click(object sender, RoutedEventArgs e)
+        private void SearchBox_KeyDown(object sender, KeyRoutedEventArgs e)
         {
-            BasicGrid.Children.Remove((UIElement)this.FindName("MultiSearchGrid"));
+            if (e.Key == Windows.System.VirtualKey.Enter)
+                SearchBox_QuerySubmitted(SearchBox, new AutoSuggestBoxQuerySubmittedEventArgs());
         }
 
+        /// <summary>
+        /// 검색 결과 리스트뷰의 항목을 클릭시 실행
+        /// </summary>
+        private void ListviewSearchResult_ItemClick(object sender, ItemClickEventArgs e)
+        {
+            var clickedItem = e.ClickedItem as SearchResultItem;
+            ViewModel.DisplayWordDetail(clickedItem);
+        }
+
+        private void BtnInform_Click(object sender, RoutedEventArgs e)
+        {
+            WebViewOpen(new Uri("https://stdict.korean.go.kr/help/popup/entry.do"));
+        }
+
+        private async void BtnHelp_Click(object sender, RoutedEventArgs e)
+        {
+            await Windows.System.Launcher.LaunchUriAsync(new Uri("https://costudio1122.blogspot.com/p/blog-page_76.html"));
+        }
+
+        private void WebViewOpen(Uri uri)
+        {
+            if (IsWebViewOpen == true)
+            {
+                Grid g = (Grid)BasicGrid.FindName("SubGrid");
+                WebView w = (WebView)g.FindName("SubWebView");
+                w.Navigate(uri);
+            }
+            else
+            {
+                var SubGrid = new Grid
+                {
+                    Name = "SubGrid",
+                    Margin = new Thickness(10, 50, 10, 10),
+                    VerticalAlignment = VerticalAlignment.Stretch,
+                    HorizontalAlignment = HorizontalAlignment.Stretch,
+                    Background = Application.Current.Resources["PageBackgroundBrush"] as SolidColorBrush,
+                    CornerRadius = new CornerRadius(8),
+                    BorderThickness = new Thickness(1),
+                    BorderBrush = Application.Current.Resources["BorderBrush"] as SolidColorBrush
+                };
+
+                var ColorBar = new Windows.UI.Xaml.Shapes.Rectangle
+                {
+                    Margin = new Thickness(0, 0, 0, 0),
+                    VerticalAlignment = VerticalAlignment.Top,
+                    HorizontalAlignment = HorizontalAlignment.Stretch,
+                    Height = 32,
+                    Fill = Application.Current.Resources["PageBackgroundBrush"] as SolidColorBrush
+                };
+                SubGrid.Children.Add(ColorBar);
+
+                var CloseBtn = new Button
+                {
+                    Name = "BtnWebViewClose",
+                    VerticalAlignment = VerticalAlignment.Top,
+                    HorizontalAlignment = HorizontalAlignment.Right,
+                    Height = 32,
+                    Width = 48,
+                    FontSize = 16,
+                    Content = "",
+                    FontFamily = new FontFamily("Segoe MDL2 Assets"),
+                    Style = Resources["AccentButtonStyle"] as Style
+                };
+                CloseBtn.Click += BtnWebViewClose_Click;
+                SubGrid.Children.Add(CloseBtn);
+
+                var SubWebView = new WebView
+                {
+                    Name = "SubWebView",
+                    Margin = new Thickness(1, 32, 1, 1),
+                    VerticalAlignment = VerticalAlignment.Stretch,
+                    HorizontalAlignment = HorizontalAlignment.Stretch,
+                    Source = uri
+                };
+                SubGrid.Children.Add(SubWebView);
+
+                BasicGrid.Children.Add(SubGrid);
+                IsWebViewOpen = true;
+            }
+        }
+
+        private void BtnWebViewClose_Click(object sender, RoutedEventArgs e)
+        {
+            BasicGrid.Children.Remove((UIElement)FindName("SubGrid"));
+            IsWebViewOpen = false;
+        }
     }
 }
